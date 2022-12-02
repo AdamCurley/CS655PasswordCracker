d8d45a36da28150b57e8aa90153ec0bf5f703a9c
arcurley <arcurley@bu.edu>
2022-12-01 20:44:13 -0500

    fix

--- PasswordCracker.java
+++ PasswordCracker.java
@@ -3,13 +3,4 @@ public class PasswordCracker {
  public static char[] ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
  public static final int PASSWORD_MAX_LENGTH = 5;
  
-  public void verify(hash, password) throws NoSuchAlgorithmException {
-     MessageDigest md = MessageDigest.getInstance("MD5");
-     md.update(password.getBytes());
-     byte[] digest = md.digest();
-     String myHash = DatatypeConverter
-       .printHexBinary(digest).toUpperCase();
-     assertThat(myHash.equals(hash)).isTrue();
- }
- 
 }
