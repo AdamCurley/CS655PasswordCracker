f8dc2c1400507e2e354853932f07735f4b02e317
AdamCurley <54548428+AdamCurley@users.noreply.github.com>
2022-12-01 20:33:23 -0500

    Update PasswordCracker.java

--- PasswordCracker.java
+++ PasswordCracker.java
@@ -1,1 +1,6 @@
+public class PasswordCracker {
 
+ public static char[] ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+ public static final int PASSWORD_MAX_LENGTH = 5;
+  
+}
