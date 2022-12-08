0e5ccc52405d0f27127a18bcf358da06798107ab
Chun Cheng <94091732+chuncheng1@users.noreply.github.com>
2022-12-08 16:19:26 -0500

    Modified scripts

---
+++ scripts/runMaster.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+wget https://raw.githubusercontent.com/AdamCurley/CS655PasswordCracker/main/src/
+javac *.java
+java Test
---
+++ scripts/runWorker3.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+wget https://raw.githubusercontent.com/AdamCurley/CS655PasswordCracker/main/src/worker/Worker.java
+javac *.java
+java Worker 58888 3
