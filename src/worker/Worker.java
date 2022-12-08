cf8c11f907c677680f0b7375e6ca32e7392cf442
Yiran-H <tina721199@126.com>
2022-12-08 14:34:48 -0500

    yiran-version3

--- src/worker/Worker.java
+++ src/worker/Worker.java
@@ -33,8 +33,8 @@ public class Worker {
                 Stop stop = new Stop();
                 Thread t = new Thread(stop);
                 t.start();
-//            } else if (input.equals("q")) {
-//                stop();
+            } else if (input.equals("q")) {
+                stop();
             } else {
                 locker.lock();
                 isRunning = true;
