7b968019862f2355ad15ebad11567b3287e200de
Chun Cheng <94091732+chuncheng1@users.noreply.github.com>
2022-12-07 19:49:39 -0500

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-printf '' | sudo -E add-apt-repository ppa:openjdk-r/ppa
+printf '' | sudo add-apt-repository ppa:openjdk-r/ppa
 sudo apt-get update
 printf 'y' | sudo apt-get install openjdk-8-jdk
