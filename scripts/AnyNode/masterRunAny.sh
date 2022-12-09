c078d96b4711741e75d95fb9e27e6fe6d3225820
Chun Cheng <94091732+chuncheng1@users.noreply.github.com>
2022-12-08 20:29:54 -0500

    Add files via upload

---
+++ scripts/AnyNode/masterRunAny.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+wget https://raw.githubusercontent.com/AdamCurley/CS655PasswordCracker/main/src/Master.java
+wget https://raw.githubusercontent.com/AdamCurley/CS655PasswordCracker/main/src/MasterHandler.java
+wget https://raw.githubusercontent.com/AdamCurley/CS655PasswordCracker/main/src/Md5.java
+wget https://raw.githubusercontent.com/AdamCurley/CS655PasswordCracker/main/src/TestForAny/TestForAny.java
+javac *.java
+java Test
