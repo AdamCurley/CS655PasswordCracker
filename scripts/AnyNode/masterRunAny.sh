2be46bdf5eadd2facfb8404d985e8b4c12eb8c4d
Chun Cheng <94091732+chuncheng1@users.noreply.github.com>
2022-12-08 20:52:56 -0500

    Update masterRunAny.sh

--- scripts/AnyNode/masterRunAny.sh
+++ scripts/AnyNode/masterRunAny.sh
@@ -2,6 +2,6 @@
 wget https://raw.githubusercontent.com/AdamCurley/CS655PasswordCracker/main/src/Master.java
 wget https://raw.githubusercontent.com/AdamCurley/CS655PasswordCracker/main/src/MasterHandler.java
 wget https://raw.githubusercontent.com/AdamCurley/CS655PasswordCracker/main/src/Md5.java
-wget https://raw.githubusercontent.com/AdamCurley/CS655PasswordCracker/main/src/TestForAny/TestForAny.java
+wget https://raw.githubusercontent.com/AdamCurley/CS655PasswordCracker/main/src/TestForAny/Test.java
 javac *.java
-java TestForAny
+java Test
